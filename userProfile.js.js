370673c935edeb0507e093e916b7e6a321194d3c
MayRosario12 <162092946+MayRosario12@users.noreply.github.com>
2024-10-30 22:01:40 +0800

    Add files via upload

---
+++ userProfile.js.js
@@ -0,0 +1,12 @@
+// Declare variables to store user information
+let fullName = "May Rosario";  // Full Name (first and last name)
+let age = 22;               // Age
+let favoriteNumber = 7;      // Favorite Number
+let favoriteColor = "blue";  // Favorite Color
+
+// Log the collected information to the console
+console.log("User Information:");
+console.log("May Rosario: " + fullName);
+console.log("Age: 22" + age);
+console.log("Favorite Number: 7" + favoriteNumber);
+console.log("Favorite Color: blue" + favoriteColor);
